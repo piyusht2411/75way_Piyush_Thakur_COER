363034945e5af72c5f4bb0494a2cd520a1c379e1
75ways <75ways@75wayss-Mac-mini.local>
2024-01-17 16:59:12 +0530

    filter

--- src/controller/filter.ts
+++ src/controller/filter.ts
@@ -7,7 +7,12 @@ export const hour = async(req:Request,res:Response)=>{
         try {
             const id = req.params.id;
             const user = await User.findById(id);
-            const hourlyTransactions = user?.transition.filter(transaction => (Date.now() - new Date(transaction.timestamp).getTime()) <= 1 * 60 * 60 * 1000);
+            const hourlyTransactions = user?.transition.filter(transaction => {
+                const timenow = new Date (new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"})).getTime();
+                const timethen:any = new Date(transaction.timestamp).getTime();
+                const differ:any = timenow - timethen;
+                return differ <= 60*60*1000;
+            });
             res.send(hourlyTransactions);
         } catch (error) {
             res.status(407).json({ message: error });
@@ -18,8 +23,13 @@ export const day = async (req:Request,res:Response)=>{
         try {
             const id = req.params.id;
             const user = await User.findById(id);
-            const dayTransactions = user?.transition.filter(transaction => (Date.now() - new Date(transaction.timestamp).getTime()) <= 24 * 60 * 60 * 1000);
-            res.send(dayTransactions);
+            const dailyTransactions = user?.transition.filter(transaction => {
+                const timenow = new Date (new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"})).getTime();
+                const timethen:any = new Date(transaction.timestamp).getTime();
+                const differ:any = timenow - timethen;
+                return differ <= 24*60*60*1000;
+            });  
+            res.send(dailyTransactions); 
         } catch (error) {
             res.status(407).json({ message: error });
         }
@@ -29,7 +39,12 @@ export const week = async(req:Request,res:Response)=>{
         try {
             const id = req.params.id;
             const user = await User.findById(id);
-            const weeklyTransactions = user?.transition.filter(transaction => (Date.now() - new Date(transaction.timestamp).getTime()) <= 7 * 24 * 60 * 60 * 1000);
+            const weeklyTransactions = user?.transition.filter(transaction => {
+                const timenow = new Date (new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"})).getTime();
+                const timethen:any = new Date(transaction.timestamp).getTime();
+                const differ:any = timenow - timethen;
+                return differ <= 7*24*60*60*1000;
+            });
             res.send(weeklyTransactions);
         } catch (error) {
             res.status(407).json({ message: error });
@@ -40,7 +55,12 @@ export const month = async(req:Request,res:Response)=>{
         try {
             const id = req.params.id;
             const user = await User.findById(id);
-            const monthlyTransactions = user?.transition.filter(transaction => (Date.now() - new Date(transaction.timestamp).getTime()) <= 30 * 24 * 60 * 60 * 1000);
+            const monthlyTransactions = user?.transition.filter(transaction => {
+                const timenow = new Date (new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"})).getTime();
+                const timethen:any = new Date(transaction.timestamp).getTime();
+                const differ:any = timenow - timethen;
+                return differ <= 30*24*60*60*1000;
+            });
             res.send(monthlyTransactions);
         } catch (error) {
             res.status(407).json({ message: error });
@@ -51,9 +71,15 @@ export const year = async(req:Request,res:Response)=>{
         try {
             const id = req.params.id;
             const user = await User.findById(id);
-            const yearlyTransactions = user?.transition.filter(transaction => (Date.now() - new Date(transaction.timestamp).getTime()) <= 365 * 24 * 60 * 60 * 1000);
+            const yearlyTransactions = user?.transition.filter(transaction => {
+                const timenow = new Date (new Date().toLocaleString("en-US", {timeZone: "Asia/Kolkata"})).getTime();
+                const timethen:any = new Date(transaction.timestamp).getTime();
+                const differ:any = timenow - timethen;
+                return differ <= 360*24*60*60*1000;
+            });
             res.send(yearlyTransactions);
         } catch (error) {
             res.status(407).json({ message: error });
         }
 }
+
